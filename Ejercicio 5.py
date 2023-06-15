4847deb6d979260aa11fc7f71a1ab6ad030bd426
CarlaKookie <car.moralesl@duocuc.cl>
2023-06-14 21:41:28 -0400

    Create Ejercicio 5.py

---
+++ Ejercicio 5.py
@@ -0,0 +1,3 @@
+#Considere la lista del ejercicio 1 y genere una segunda lista con los elementos
+#ordenados en forma ascendente y una tercera lista con los elementos ordenados descendentemente.Mostrar los tres resultados
+
